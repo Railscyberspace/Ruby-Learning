7af2e1b60ceddb64603488c0e40da499be1f876c
Rails Abam <abamsunday5@gmail.com>
2022-09-19 20:12:01 +0100

    added new javascript changes if statments and swtich statments

---
+++ variable.js
@@ -0,0 +1,30 @@
+
+
+let score = 8;
+console.log("Mid-level skills:" > score);
+
+
+let age = 10;
+if(age >= 65){
+    console.log("You get your income from your pension")
+}else if(age < 65 && age >= 18){
+    console.log( "Each month you get a salary")
+}else if(age < 18){
+    console.log("You get an allowance")
+}else{
+    console.log( "The value of the age variable is not numerical")
+}
+
+
+let day = "monday"
+switch(day){
+      case "monday":
+      console.log("it monday")
+      break;
+      case "tuesday":
+      console.log("it Tuesday")
+      break;
+      case "wedneday":
+      default:
+      console.log("not a day")
+}
