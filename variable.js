a28ac8d6d1896125c258518499bfbe7399cab99a
Rails Abam <abamsunday5@gmail.com>
2022-09-19 23:57:29 +0100

    added new javascript changes loops,while loopd,for loop

--- variable.js
+++ variable.js
@@ -28,3 +28,21 @@ switch(day){
       default:
       console.log("not a day")
 }
+
+
+
+for (var i = 1; i <= 10; i++) {
+    switch(i) {
+        case 1:
+            console.log("Gold medal")
+            break
+        case 2:
+            console.log("Silver medal")
+            break
+        case 3:
+            console.log("Bronze medal")
+            break
+        default:
+            console.log(i)
+    }
+}
