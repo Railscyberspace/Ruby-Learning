dfa1449f7243b22eceb706ce2d4f21be3d37283a
Rails Abam <railsdevabam@gmail.com>
2022-09-24 00:55:58 +0100

    added new javascript changes and added files like methods, practics, opp.js

--- practiceloop.js
+++ practiceloop.js
@@ -1,10 +1,25 @@
 //To loop over  an array and assign each to a particular number
 
 
-list = ["rails", "ruby", "pearl", "kingsly"];
-list.reverse()
-//basically rails = 3, "kingsly = 2", pearl =0 , ruby 2 
+// list = ["rails", "ruby", "pearl", "kingsly"];
+// list.reverse()
+// //basically rails = 3, "kingsly = 2", pearl =0 , ruby 2 
 
-for(let i = 0; i < list.length; i++){
-     console.log(list[i], i)
-    }
+// for(let i = 0; i < list.length; i++){
+//      console.log(list[i], i)
+//     }
+
+
+// OOP
+
+class Train{
+     constructor(color, ligthOn){
+  this.color = color;
+  this.ligthOn = ligthOn
+     }
+}
+
+let myTrain = new Train("pink", true)
+let mySecond = new Train("green0", false)
+let myThird = new Train("pink", "No")
+console.log(myTrain, mySecond, myThird)
