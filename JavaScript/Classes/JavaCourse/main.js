680c377e8072247a19f03ec310bd7ef4ec281201
Rails Abam <railsdevabam@gmail.com>
2022-09-29 13:51:25 +0100

    added a javascript files

--- JavaScript/Classes/JavaCourse/main.js
+++ JavaScript/Classes/JavaCourse/main.js
@@ -2,12 +2,17 @@
 // myHeading.textContent = "Hello world!";
 
 const myButton = document.querySelector("button");
-
+const myHeading = document.querySelector("h1")
 
 function setUserName(){
     const myName = prompt("please Enter your name: ");
-    localStorage.setItem("name: ", myName);
-    myHeading.textContent = `Hello I'm making progress ${myName}`
+    if(!setUserName){
+        setUserName();
+    }else{
+        localStorage.setItem("name: ", myName);
+        myHeading.textContent = `Hello I'm making progress ${myName}`
+    }
+    
 }
 
 if(!localStorage.getItem("name")){
@@ -16,6 +21,8 @@ if(!localStorage.getItem("name")){
     const storeName = localStorage.getItem("name");
     myHeading.textContent(`Hello is the not going well ${storeName}`)
 }
+
+
 myButton.onclick =() =>{
     setUserName();
 }
