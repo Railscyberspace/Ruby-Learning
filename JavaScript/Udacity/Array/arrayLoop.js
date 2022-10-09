06d22e169891b836d402d0c0d398a35389dc296d
Rails Abam <railsdevabam@gmail.com>
2022-10-09 23:33:28 +0100

    commiting array changes and push method  and foreach method

--- JavaScript/Udacity/Array/arrayLoop.js
+++ JavaScript/Udacity/Array/arrayLoop.js
@@ -22,5 +22,31 @@ function myAwesomeFunction(element, index, array) {
   console.log("Index: ", index);
   console.log("Array: ", array);
 }
-
 myArray.forEach(myAwesomeFunction);
+
+
+
+const words = ["cat", "in", "hat"];
+words.forEach(function(word, num, all){
+    console.log("words of string " + num + " is " + all.toString())
+})
+
+
+const test = [12, 929, 11, 3, 199, 1000, 7, 1, 24, 37, 4, 19, 300, 3775, 299,36, 209, 148, 169, 299, 6, 109, 20, 58, 139, 59, 3, 1, 139];
+
+//const miniTest = [12, 29, 11, 3]
+test.forEach(function(allDivisble, notDivisa){
+    if(allDivisble  % 3 === 0 ){;
+         test[notDivisa] = test[notDivisa] + 100;
+     
+    }   
+      console.log(test)
+    
+})
+
+test.forEach(function (item, index) {
+    if (item % 3 === 0) {
+      test[index] = test[index] + 100;
+    }
+  });
+  console.log(test);
