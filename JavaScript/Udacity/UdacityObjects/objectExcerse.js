10b4c7c6cd283a90f794ca2640582d70c91a90f9
Rails Abam <railsdevabam@gmail.com>
2022-10-12 17:17:17 +0100

    commiting object excerise

---
+++ JavaScript/Udacity/UdacityObjects/objectExcerse.js
@@ -0,0 +1,26 @@
+
+const facebookProfile = {
+    name: "Rails  Abam",
+    friends: 1.324,
+    message: ["Welcome to my ", "list of friends"],
+
+ 
+    postMessage: function message () {
+        
+    },
+
+
+    deleteMessage: function index () {
+        
+    },
+
+   function: addFriend (){
+
+   },
+
+    
+
+    function: removeFriend() {
+
+    }
+}
