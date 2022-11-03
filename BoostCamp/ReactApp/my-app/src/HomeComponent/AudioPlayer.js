7ebae2a64cf88c7dc69aebef87402e9ad76051d3
Rails Abam <railsdevabam@gmail.com>
2022-11-03 07:25:34 +0100

    commiting changes on on images and vidoes

---
+++ BoostCamp/ReactApp/my-app/src/HomeComponent/AudioPlayer.js
@@ -0,0 +1,32 @@
+import Home  from "./Home"
+import React from "react"
+
+
+
+const bird1 = new Audio(
+    "https://upload.wikimedia.org/wikipedia/commons/9/9b/Hydroprogne_caspia_-_Caspian_Tern_XC432679.mp3"
+  );
+
+  const bird2 = new Audio(
+    "https://upload.wikimedia.org/wikipedia/commons/b/b5/Hydroprogne_caspia_-_Caspian_Tern_XC432881.mp3"
+  );
+
+function toggle1(){
+    if(bird1.paused){
+        bird1.play()
+    }else{
+        bird1.pause()
+    }
+}
+
+function toggle2(){
+    if(bird1.paused){
+        bird1.play()
+    }else{
+        bird1.pause()
+    }
+}
+
+
+
+export default {toggle1, toggle2};
--- BoostCamp/ReactApp/my-app/src/HomeComponent/Home.js
+++ BoostCamp/ReactApp/my-app/src/HomeComponent/Home.js
@@ -2,20 +2,27 @@ import  carrots from './Assests/carrots.jpeg'
 import DisplayMessage from "./MessageButton";
 import ReactPlayer from "react-player";
 import React from 'react';
+import {toggle1}  from './AudioPlayer';
+import toggle2 from './AudioPlayer'
 
 
 function Home(){
     const vdmedia = "https://www.youtube.com/watch?v=MO3BnlMg5rk";
+
+
+
     return(
         <div>
             <DisplayMessage/>
              <img src =  {carrots} alt ='Carrota picture'/> 
              <div className='container'>
              <h2>Media player:</h2>
-             <ReactPlayer url={vdmedia} playing ={false} volume ={0.6} /> 
-
+             <ReactPlayer url={vdmedia} playing ={false} volume ={0.6} controls ={true}/> 
              </div>
-           
+             <button onClick={toggle1}>toggle1</button>
+             <button onClick={toggle2}> toggle2</button>
+
+            
         </div>
     );
 };
