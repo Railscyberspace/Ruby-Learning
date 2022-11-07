b151400069111860ec732b7b5b2f9da1bb66b75c
Rails Abam <railsdevabam@gmail.com>
2022-11-07 22:48:37 +0100

    commiting changes  on images

--- BoostCamp/ReactApp/my-app/src/ProductComponent/Inverters.js
+++ BoostCamp/ReactApp/my-app/src/ProductComponent/Inverters.js
@@ -10,43 +10,42 @@ import xtrapower from '../Images/xtrapower.png';
 function Inverters(){
     return(
         <div>
-        <div className="container text-center">
-            <div className="row align-items-start">
-                <div className="col">
-                <img src= { inverter5kvapc} height={200} width ={200} alt = "Hybrid inveter"/>
-                </div>
-                <div class="col">
-                <img src={lithiumbattery} height={200} width ={200} alt = 'Mppt charge controller'/>
-                </div>
-                <div class="col">
-                <img src={chargecontroller} height={200} width ={200} alt = 'Mppt charge controller'/>
-                </div>
-            </div>
-        </div>
+            <div class="container text-center">
+  <div class="row align-items-start">
+    <div class="col">
+    <img src= { inverter5kvapc} height={200} width ={200} alt = "Hybrid inveter"/>
+    </div>
+    <div class="col">
+    <img src={lithiumbattery} height={200} width ={200} alt = 'Mppt charge controller'/>
+    </div>
+    <div class="col">
+    <img src={chargecontroller} height={200} width ={200} alt = 'Mppt charge controller'/>
+    </div>
+  </div>
+  <div class="row align-items-center">
+    <div class="col">
+    <img src={powerhybrid} height ={200} width ={200} alt = "powerhybrid"/>
 
-        <div class="row align-items-center">
-            <div class="col">
-            <img src={powerhybrid} height ={200} width ={200} alt = "powerhybrid"/>
-            </div>
-            <div class="col">
-            <img src={smartmpptsolarcharger} height ={200} width ={200} alt = "powerhybrid"/>
-            </div>
-            <div class="col">
-            <img src ={xtrapowerMPPT} height={200} width ={200} alt="xtrapowerMPPT"/>
-            </div>
-        </div>
-
-        <div class="row align-items-end">
-            <div class="col">
-            <img src={LITHIUMBATTERY} height ={200} width ={200} alt ="LITHIUMBATTERY"/>
-            </div>
-            <div class="col">
-            <img src ={xtrapower} height ={200} width ={200} alt ="xtrapower"/>
-            </div>
-            <div class="col">
-            <img src={lithiumbattery} height = {200} width ={200} alt = 'lithiumbattery'/>
-            </div>
-        </div>
+    </div>
+    <div class="col">
+    <img src={smartmpptsolarcharger} height ={200} width ={200} alt = "powerhybrid"/>
+    </div>
+    <div class="col">
+    <img src ={xtrapowerMPPT} height={200} width ={200} alt="xtrapowerMPPT"/>
+    </div>
+  </div>
+  <div class="row align-items-end">
+    <div class="col">
+    <img src={LITHIUMBATTERY} height ={200} width ={200} alt ="LITHIUMBATTERY"/>
+    </div>
+    <div class="col">
+    <img src ={xtrapower} height ={200} width ={200} alt ="xtrapower"/>
+    </div>
+    <div class="col">
+    <img src={lithiumbattery} height = {200} width ={200} alt = 'lithiumbattery'/>
+    </div>
+  </div>
+</div>
     </div>
 
     )
