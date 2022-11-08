b995a6fc4e92e707191adcbfa5f943e479c5208f
Rails Abam <railsdevabam@gmail.com>
2022-11-08 23:22:12 +0100

    commiting changes on inverter component and added card button

--- BoostCamp/ReactApp/my-app/src/ProductComponent/Inverters.js
+++ BoostCamp/ReactApp/my-app/src/ProductComponent/Inverters.js
@@ -6,18 +6,26 @@ import smartmpptsolarcharger from '../Images/smartmpptsolarcharger.png';
 import lithiumbatteries from '../Images/lithiumbatteries.png';
 import xtrapowerMPPT from '../Images/xtrapowerMPPT.png';
 import xtrapower from '../Images/xtrapower.png';
+import Crown48v from '../Images/Crown48v.png';
+import femicareInverter48v from '../Images/femicareInverter48v.png';
+import VimINverter48v from '../Images/VimINverter48v.png';
+import Proton24V from '../Images/Proton24V.png';
+import ProtonHybrid48v from '../Images/ProtonHybrid48v.png';
+import ProtonHyrbrid48v from '../Images/ProtonHyrbrid48v.jpeg';
+
+
 
 function Inverters(){
     return(
-        <div>
-            <div class="container text-center">
+<div>
+<div class="container text-center">
   <div class="row align-items-start">
     <div class="col"> 
         <div class="card" width={18}>
         <img src= { inverter5kvapc} height={200} width ={200} alt = "Hybrid inveter" className='rounded float-start'/>
         <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+            <h5 class="card-title">xtrapower Hybrid</h5>
+            <h6 class="card-subtitle mb-2 text-muted">5kva 48v</h6>
             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
             <span>N580,000.00</span>
             <button type="button" class="btn btn-secondary">Add to Cart</button>
@@ -28,8 +36,8 @@ function Inverters(){
             <div class="card" width={18}>
             <img src={lithiumbattery} height={200} width ={200} alt = 'Mppt charge controller' className='rounded float-start'/>
             <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                <h5 class="card-title">xtrapower Hybrid </h5>
+                <h6 class="card-subtitle mb-2 text-muted">7.5kva 48v</h6>
                 <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                 <span>N580,000.00</span>
                 <button type="button" class="btn btn-secondary">Add to Cart</button>
@@ -40,8 +48,8 @@ function Inverters(){
             <div class="card" width={18}>
             <img src={chargecontroller} height={200} width ={200} alt = 'Mppt charge controller' className='rounded float-start'/>
             <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                <h5 class="card-title">xtrapower charge controller</h5>
+                <h6 class="card-subtitle mb-2 text-muted">12v-48v 70A </h6>
                 <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                 <span>N580,000.00</span>
                 <button type="button" class="btn btn-secondary">Add to Cart</button>
@@ -54,8 +62,8 @@ function Inverters(){
              <div class="card" width={18}>
              <img src={powerhybrid} height ={200} width ={200} alt = "powerhybrid" className='rounded float-start'/>
             <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                <h5 class="card-title">xtrapower Hybrid series</h5>
+                <h6 class="card-subtitle mb-2 text-muted">4kva 48v</h6>
                 <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                 <span>N580,000.00</span>
                 <button type="button" class="btn btn-secondary">Add to Cart</button>
@@ -66,8 +74,8 @@ function Inverters(){
             <div class="card" width={18} >
             <img src={smartmpptsolarcharger} height ={200} width ={200} alt = "powerhybrid" className='rounded float-start'/>
             <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                <h5 class="card-title">xtrapower chargecontroller </h5>
+                <h6 class="card-subtitle mb-2 text-muted">96v-300v 100A series</h6>
                 <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                 <span>N580,000.00</span>
                 <button type="button" class="btn btn-secondary">Add to Cart</button>
@@ -78,10 +86,10 @@ function Inverters(){
         <div class="card" width={18}>
                 <img src ={xtrapowerMPPT} height={200} width ={200} alt="xtrapowerMPPT" className='rounded float-start'/>
                    <div class="card-body">
-                      <h5 class="card-title">Card title</h5>
-                          <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                      <h5 class="card-title">xtrapower touch screen chargecontroller</h5>
+                          <h6 class="card-subtitle mb-2 text-muted">48v 100A touch screen series</h6>
                           <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                          <span>N580,000.00</span>
+                          <span>N380,000.00</span>
                           <button type="button" class="btn btn-secondary">Add to Cart</button>
             </div>
             </div>
@@ -125,8 +133,88 @@ function Inverters(){
        </div>
     </div>
   </div>
-</div>
+
+  <div class="row align-items-center">
+    <div class="col">
+                <div class="card" width={18}>
+                <img src ={Crown48v} height ={200} width ={200} alt="Crown 48v Inverter"/>
+               <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <span>N580,000.00</span>
+                <button type="button" class="btn btn-secondary">Add to Cart</button>
+            </div>
+            </div>
     </div>
+    <div class="col">
+            <div class="card" width={18}>
+            <img src ={femicareInverter48v}  height ={200} width ={200} alt ="Crown 48v 5kva Inverter"/> 
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <span>N580,000.00</span>
+                <button type="button" class="btn btn-secondary">Add to Cart</button>
+            </div>
+            </div>
+    </div>
+    <div class="col">
+            <div class="card" width={18}>
+            <img src={VimINverter48v} height={200} width ={200} alt = 'femicare Inverter 48v 3.5kva'/> 
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <span>N580,000.00</span>
+                <button type="button" class="btn btn-secondary">Add to Cart</button>
+            </div>
+         </div>
+     </div>
+</div>
+<div class="row align-items-end">
+            <div class="col">
+            <div class="card" width={18}>
+                <img src={Proton24V} height={200} width ={200} alt = "Proton hyride inverter"/>
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <span>N580,000.00</span>
+                <button type="button" class="btn btn-secondary">Add to Cart</button>
+            </div>
+            </div>
+          </div>
+    <div class="col">
+    <div class="card" width={18}>
+        <img src ={ProtonHybrid48v} height={200} width={200} alt ="proton Hybrid inverter series"/>
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <span>N580,000.00</span>
+            <button type="button" class="btn btn-secondary">Add to Cart</button>
+        </div>
+        </div>
+    </div>
+    <div class="col">
+                <div class="card" width={18}>
+                    <img src ={ProtonHyrbrid48v} height={200} width={200} alt ="Hybide series inverter"/>
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <span>N580,000.00</span>
+                    <button type="button" class="btn btn-secondary">Add to Cart</button>
+            </div>
+        </div>
+    </div>
+  </div>
+
+
+
+</div>
+</div>
 
     )
 }
