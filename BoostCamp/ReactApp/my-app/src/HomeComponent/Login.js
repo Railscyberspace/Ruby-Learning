b77aabbae1d6d2bbcc01bbaed7cd1b0f3ff3d09c
Rails Abam <railsdevabam@gmail.com>
2022-11-09 13:10:07 +0100

    commiting changes on inverter component and added card button on batteries component and styles login

--- BoostCamp/ReactApp/my-app/src/HomeComponent/Login.js
+++ BoostCamp/ReactApp/my-app/src/HomeComponent/Login.js
@@ -1,30 +1,30 @@
 import '../StyleComponent/Login.css';
 
 import {useState} from 'react';
-function Login(){
+function Login(){  
 
     const [form, setForm] = useState({
-        firstname:"Rails",  lastName: "Abam", Email: "abam@gmail.com", Password: ''
+        firstname:"",  lastName: '', Email: '', Password: ''
     });
     return(
         <div className="container form">
-                <h2>Login Here</h2>
                 <div className='form-container'>
+                <h2>Login Here</h2>
                 <div className='mb-3'>
                     <label for  ="exampleFormControlInput1" className ="form-label">firstname  </label>
                     <input  type="text" className="form-control" id="exampleFormControlInput1" value = {form.firstname} onChange ={e =>{setForm({...form, firstname: e.target.value})}}/>
                 </div>
-                <div className='mb-3'>
+                <div className  = "col form-group">
                     <label for  ="exampleFormControlInput1" className ="form-label">lastName </label>
                     <input type="text" className="form-control" id="exampleFormControlInput1" value = {form.lastName} onChange ={e =>{setForm({...form, lastName: e.target.value})}}/>
                 </div>
-                <div className='mb-3'>  
+                <div className='mb-3 col form-group"'>  
                     <label for  ="exampleFormControlInput1" className ="form-label">Email</label>
-                        <input type="email" className="form-control" id="exampleFormControlInput1"  value = {form.Email}onChange ={e =>{setForm({...form,Email: e.target.value})}}/>
+                        <input type="email" className="form-control" id="exampleFormControlInput1"   placeholder='Email' value = {form.label}onChange ={e =>{setForm({...form,Email: e.target.value})}}/>
                 </div>
-                <div className='mb-3'>  
+                <div className='mb-3 col form-group '>  
                     <label for  ="exampleFormControlInput1" className ="form-label">Password</label>
-                    <input type="password" className="form-control" id="exampleFormControlInput1"  value = {form.Email}onChange ={e =>{setForm({...form,Password: e.target.value})}}/>
+                    <input type="password" className="form-control input-fill" id="exampleFormControlInput1"  placeholder='Password' value = {form.label}onChange ={e =>{setForm({...form,Password: e.target.value})}}/>
                 </div>
 
              </div>
