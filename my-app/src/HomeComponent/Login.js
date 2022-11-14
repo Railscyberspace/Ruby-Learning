8306651eb6c8d76c3c192bdd3bb973c0313da090
Rails Abam <railsdevabam@gmail.com>
2022-11-15 00:01:00 +0100

    commiting form changes

--- my-app/src/HomeComponent/Login.js
+++ my-app/src/HomeComponent/Login.js
@@ -1,34 +1,53 @@
 import '../StyleComponent/Login.scss';
 
-import {useState} from 'react';
+// import {useState} from 'react';
 function Login(){  
 
-    const [form, setForm] = useState({
-        firstname:"",  lastName: '', Email: '', Password: ''
-    });
+    // const [form, setForm] = useState({
+    //     firstname:"",  lastName: '', Email: '', Password: ''
+    // });
     return(
         <div className="container form">
-                <div className='form-container'>
-                <h2>Login Here</h2>
-                <div className='mb-3'>
-                    <label for  ="exampleFormControlInput1" className ="form-label">firstname  </label>
-                    <input  type="text" className="form-control" id="exampleFormControlInput1" placeholder='Firstname' value = {form.firstname} onChange ={e =>{setForm({...form, firstname: e.target.value})}}/>
+            <div className='container'>
+            <div className="row g-3">
+                <div className="col">
+                    <input type="text" className="form-control" placeholder="First name" aria-label="First name"/>
                 </div>
-                <div className  = "col form-group">
-                    <label for  ="exampleFormControlInput1" className ="form-label">lastName </label>
-                    <input type="text" className="form-control" id="exampleFormControlInput1" placeholder='Lastname' value = {form.lastName} onChange ={e =>{setForm({...form, lastName: e.target.value})}}/>
+                <div className="col">
+                    <input type="text" className="form-control" placeholder="Last name" aria-label="Last name"/>
                 </div>
-                <div className='mb-3 col form-group"'>  
-                    <label for  ="exampleFormControlInput1" className ="form-label">Email</label>
-                        <input type="email" className="form-control" id="exampleFormControlInput1"   placeholder='Email' value = {form.label}onChange ={e =>{setForm({...form,Email: e.target.value})}}/>
+           </div>
+           <div className="row g-3">
+                <div className="col">
+                    <input type="text" className="form-control" placeholder="Email" aria-label="First name"/>
                 </div>
-                <div className='mb-3 col form-group '>  
-                    <label for  ="exampleFormControlInput1" className ="form-label">Password</label>
-                    <input type="password" className="form-control input-fill" id="exampleFormControlInput1"  placeholder='Password' onChange ={e =>{setForm({...form,Password: e.target})}}/>
+                <div className="col">
+                    <input type="text" className="form-control" placeholder="Phone" aria-label="Last name"/>
                 </div>
-
-             </div>
-</div>
+           </div>
+           <div className="row g-3">
+                <div className="col">
+                    <input type="text" className="form-control" placeholder="State" aria-label="First name"/>
+                </div>
+                <div className="col">
+                    <input type="text" className="form-control" placeholder="Local-Govt" aria-label="Last name"/>
+                </div>
+           </div>
+           <div className="row g-3">
+                <div className="col">
+                    <input type="text" className="form-control" placeholder="Password" aria-label="First name"/>
+                </div>
+                <div className="col">
+                    <input type="text" className="form-control" placeholder="Confirm Password" aria-label="Last name"/>
+                </div>
+           </div>
+                <div className="col">
+                    <button type="submit" className="form-control" >Submit</button>
+                 </div>
+            </div>
+           
+ 
+        </div>
        
     );
 };
