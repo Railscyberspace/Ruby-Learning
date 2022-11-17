cb6e81ef64e26a4c25b982f7436a6263741ac3a3
Rails Abam <railsdevabam@gmail.com>
2022-11-17 06:35:25 +0100

    commiting changes

--- my-app/src/HomeComponent/Login.js
+++ my-app/src/HomeComponent/Login.js
@@ -9,121 +9,67 @@ function Login(){
     return(
         <div>
 
-                <div class="container text-center Register-container">
-                    <div class="row align-items-start" >
-                     <div  className ="container-login">
-                            <div class="col">
+            <div className = "form-container">
+                <div className='container-login register'>
+                    <h4 className='login-login text'>Create An Account</h4>
+                    <p className = 'welcome-message text'>Welcome to your ons stop for power backups</p>
+                      <div className='row'>
+                        <div className='col form-group'>
                             <label>firstname</label>
-                                  <input type="text" className="form-control input-fill" id = 'place'  placeholder="First name" aria-label="First name"/>
-                            </div>
-                            <div class="col">
-                            <label>lastname</label>
-                                <input type="text" className="form-control input-fill" id = 'place' placeholder="Last name" aria-label="Last name"/>
-                            </div>
+                            <input type='text' className = 'form-control input-fill' id ='control' placeholder = 'firstname'/>
                         </div>
-                        <div class="row align-items-center">
-                            <div class="col">
-                            <label>email</label>
-                                   <input type="email" className="form-control input-fill "id = 'place' placeholder="Email" aria-label="First name"/>
-                            </div>
-                            <div class="col">
+                    </div>
+                    <div className='row'>
+                        <div className='col form-group'>
+                            <label>lastName</label>
+                            <input type='text' className = 'form-control input-fill' id ='control' placeholder = 'lastname'/>
+                        </div>
+                    </div>
+                    <div className='row'>
+                        <div className='col form-group'>
+                            <label>Email</label>
+                            <input type='email' className = 'form-control input-fill' id ='control' placeholder = 'email'/>
+                        </div>
+                    </div>
+                    <div className='row'>
+                        <div className='col form-group'>
                             <label>Phone</label>
-                                   <input type="number" className="form-control input-fill"  id = 'place' placeholder="Phone" aria-label="First name"/>
-                            </div>
+                            <input type='number' className = 'form-control input-fill' id ='control' placeholder = 'phone'/>
                         </div>
-                        <div class="row align-items-end">
-                            <div class="col">
-                            <label>State</label>
-                                  <input type="text" className="form-control input-fill"  id = 'place' placeholder="state" aria-label="First name"/>
-                            </div>
-                            <div class="col">
+                    </div>
+
+                    <div className='row'>
+                        <div className='col form-group'>
+                            <label>state</label>
+                            <input type='text' className = 'form-control input-fill' id ='control' placeholder = 'state'/>
+                        </div>
+                    </div>
+
+                    <div className='row'>
+                        <div className='col form-group'>
                             <label>Local-Govt</label>
-                                  <input type="text" className="form-control input-fill" id = 'place'  placeholder="Local-Govt" aria-label="First name"/>
-                            </div>
-                         </div>
-
-                            <div class="row align-items-end">
-                                <div class="col">
-                                <label>password</label>
-                                           <input type="password" className="form-control input-fill" id = 'place'  placeholder="Password" aria-label="Last name"/>
-                                </div>
-                                <div class="col">
-                                <label>confirm-password</label>
-                                         <input type="password" className="form-control input-fill" id = 'place'  placeholder="Confirm Password" aria-label="Last name"/>
-                                </div>
-                                <div class="col">
-                                            <button type='submit' className='submit'>Submit</button>
-                                 </div>
-                                </div>
-              </div>          
-                 </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-{/* 
-                <div className="row">
-                    <div className="col">
-                        <label>firstname</label>
-                        <input type="text" className="form-control input-fill" id = 'place'  placeholder="First name" aria-label="First name"/>
+                            <input type='text' className = 'form-control input-fill' id ='control' placeholder = 'local-govt'/>
+                        </div>
                     </div>
-                    <div className="col">
-                    <label>lastname</label>
-                        <input type="text" className="form-control input-fill" id = 'place' placeholder="Last name" aria-label="Last name"/>
+
+                    <div className='row'>
+                        <div className='col form-group'>
+                            <label>Password</label>
+                            <input type='password' className = 'form-control input-fill' id ='control' placeholder = 'password'/>
+                        </div>
                     </div>
+                    <div className='row'>
+                        <div className='col form-group'>
+                            <label>confirm password</label>
+                            <input type='password' className = 'form-control input-fill' id ='control' placeholder = 'confirm password'/>
+                        </div>
+                    </div>
+      
+                </div>
+
             </div>
-           <div className="row align-items-center">
-                <div className="col head">
-                <label>email</label>
-                    <input type="text" className="form-control input-fill "id = 'place' placeholder="Email" aria-label="First name"/>
-                </div>
-                <div className="col">
-                    <label>phone</label>
-                    <input type="text" className="form-control input-fill"  id = 'place'  placeholder="Phone" aria-label="Last name"/>
-                </div>
-           </div>
-           <div className="row">
-                <div className="col">
-                <label>State</label>
-                    <input type="text" className="form-control input-fill"  id = 'place' placeholder="State" aria-label="First name"/>
-                </div>
-                <div className="col">
-                <label>local-Govt</label>
-                    <input type="text" className="form-control input-fill" id = 'place' placeholder="Local-Govt" aria-label="Last name"/>
-                </div>
-           </div>
-           <div className="row ">
-                <div className="col">
-                <label>Password</label>
-                    <input type="text" className="form-control input-fill" id = 'place'  placeholder="Password" aria-label="First name"/>
-                </div>
-                <div className="col">
-                <label>confirm-password</label>
-                    <input type="text" className="form-control input-fill" id = 'place'  placeholder="Confirm Password" aria-label="Last name"/>
-                </div>
-           </div>
-                <div className="col submit">
-                    <button type="submit" className="btn btn-secondary btn-lg" >Submit</button>
-    </div> *
 
-     <div className='Register-container'>
-             <div className='container-login'>
-                    <h2 className='head-text'>Create An Aaccount</h2>
-                    <p className='welcome-text'>Enter your details to Register</p>
-
-    */}
-           
+               
  
     </div>
        
