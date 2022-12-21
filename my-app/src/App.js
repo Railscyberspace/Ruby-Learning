c8dfc800e674b38e145b43b037f1a5a8a3d8f7a7
Rails Abam <railsdevabam@gmail.com>
2022-12-21 09:54:53 +0100

    commiting changes on deleted files

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -21,54 +21,7 @@ import SignUp from './ProductComponent/SignUp';
 
 
 function App() {
-  // const data1 = [
-    //     {
-    //     id: 1,
-    //     title: "railprofile",
-    //     discription: "github",
-    //     image:"https://github.com/RailsAbam",
-    //     role: "junior dev",
-    
-    //      },
-    //       {
-    //         id:2, 
-    //         title:"coraprofile",
-    //         discription: "github",
-    //         image:"https://github.com/yocontra/react-responsive/commits?author=yocontra",
-    //         role:"senior dev",container
-    
-    //     },
-    //     {
-    //     id:3, 
-    //         title:"pekerprofile",
-    //         discription: "github",
-    //         image:"https://github.com/pekeler",
-    //         role:"peker dev",
-    
-    //     }
-    // 
-    
-    // {
-    //   name: "Chocolate Cake",
-    //   calories: 400,
-    //   createdAt: "2022-09-01",
-    // },
-    // {
-    //   name: "Ice Cream",
-    //   calories: 200,
-    //   createdAt: "2022-01-02",
-    // },
-    // {
-    //   name: "Tiramisu",
-    //   calories: 300,
-    //   createdAt: "2021-10-03",
-    // },
-    // {
-    //   name: "Cheesecake",
-    //   calories: 600,
-    //   createdAt: "2022-01-04",
-    // },
-    // ]
+
 
   
   return (
