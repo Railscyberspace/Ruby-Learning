11d05e2dc25cde2cfd230aaa98d9dd5788a61def
Rails Abam <railsdevabam@gmail.com>
2022-12-21 07:23:52 +0100

    commiting changes on the switch file

---
+++ my-app/src/Switch/Switch.js
@@ -0,0 +1,15 @@
+
+import  {useTheme} from '../HomeComponent/ThemeContext';
+
+
+const Switch  = () => {
+    const {theme } = useTheme();
+    return (
+        <label className='switch'>
+            <input type ="checkbox" checked={theme === "light"}/>
+            <span className='slider-around'/>
+        </label>
+    );
+};
+
+export default Switch;
