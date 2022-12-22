c9ce5cc404244383d0fb0995773f0ffb8700a0ff
Rails Abam <railsdevabam@gmail.com>
2022-12-22 06:06:03 +0100

    commiting changes on deleted files

--- my-app/src/AdvanceReact/useHook.js
+++ my-app/src/AdvanceReact/useHook.js
@@ -14,4 +14,21 @@ function useHook(){
         </div>
     )
 };
-export default useHook;
+//Correct way to update usestate 
+export default function App() { 
+  const [greeting, setGreeting] = useState({ greet: "Hello, World" }); 
+  console.log(greeting, setGreeting); 
+ 
+  function updateGreeting() { 
+    const newGreeting = {...greeting}; 
+    newGreeting.greet = "Hello, World-Wide Web"; 
+    setGreeting(newGreeting); 
+  } 
+ 
+  return ( 
+    <div> 
+      <h1>{greeting.greet}</h1> 
+      <button onClick={updateGreeting}>Update greeting</button> 
+    </div> 
+  ); 
+} 
